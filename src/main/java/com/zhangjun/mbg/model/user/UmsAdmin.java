5e13bd42210d7a5c93765e61ce2a8351e9ff0daf
zhangjun <983123466@qq.com>
2025-05-09 22:20:18 +0800

    第三次提交，新增支持mongodb架构

--- src/main/java/com/zhangjun/mbg/model/user/UmsAdmin.java
+++ src/main/java/com/zhangjun/mbg/model/user/UmsAdmin.java
@@ -61,24 +61,4 @@ public class UmsAdmin implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", username=").append(username);
-        sb.append(", password=").append(password);
-        sb.append(", icon=").append(icon);
-        sb.append(", email=").append(email);
-        sb.append(", nickName=").append(nickName);
-        sb.append(", note=").append(note);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", loginTime=").append(loginTime);
-        sb.append(", status=").append(status);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
 }
